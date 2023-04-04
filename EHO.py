e1e1b978bbe9c34191b5b1b314f6c477a2e5316c
TolmachevValerij <98163361+TolmachevValerij@users.noreply.github.com>
2023-04-04 16:22:48 +0500

    Add files via upload

---
+++ EHO.py
@@ -0,0 +1,27 @@
+# Импортируем необходимые классы.
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def echo(update, context):
+    await update.message.reply_text('Я получил сообщение: '+update.message.text)
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    text_handler = MessageHandler(filters.TEXT, echo)
+    application.add_handler(text_handler)
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
